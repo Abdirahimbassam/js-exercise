9da4c40d502a8c1e49d9401e61866f321918863c
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-01-21 13:47:43 +0300

    Submitted Exercise 17 in javascript exercise

---
+++ Exercise 17/js-exercise-17.js
@@ -0,0 +1,15 @@
+
+let Temperature = 28;
+
+if ( Temperature < 0 ) {
+    console.log("Very Cold")
+
+} else if (Temperature >= 0 && Temperature <15 ) {
+    console.log("Cold")
+
+} else if (Temperature >= 15 && Temperature <= 25) {
+    console.log("Warm")
+
+} else {
+    console.log("Hot")
+}
