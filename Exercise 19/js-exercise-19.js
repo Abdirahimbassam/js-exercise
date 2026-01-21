e056fa58198ce50d38d6a01c3b6939a92026b0f1
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-01-21 15:26:33 +0300

    Submitted Exercise 19 in javascript exercise

---
+++ Exercise 19/js-exercise-19.js
@@ -0,0 +1,15 @@
+const number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+// funtion
+
+number.forEach(function(number){
+    console.log(number)
+})
+
+console.log("----------------------------------");
+
+// array function
+
+number.forEach((number)=>{
+    console.log(number)
+})
