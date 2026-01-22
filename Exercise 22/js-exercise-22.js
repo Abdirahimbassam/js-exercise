566db53eb484395ab9b65a3e0582a6ad90adb1ba
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-01-22 14:06:43 +0300

    Submitted Exercise 22 in javascript exercise

---
+++ Exercise 22/js-exercise-22.js
@@ -0,0 +1,23 @@
+
+const colors = ["red", "green", "blue"];
+
+// old version array
+console.log(colors)
+
+console.log("-------------")
+
+// by index
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+console.log("-------------");
+
+// array destructuring
+
+const color = ["White", "Orange", "Yellow"];
+
+const [first ,  , third] = color
+
+console.log(first);
+console.log(third);
