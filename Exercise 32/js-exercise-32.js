780ee901c60a6f4e6fb131995d101a5b07bbc954
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-01-25 18:35:41 +0300

    Submitted Exercise 32 in javascript exercise

---
+++ Exercise 32/js-exercise-32.js
@@ -0,0 +1,13 @@
+
+const first = document.querySelector('#first-id');
+console.log(first);
+
+
+const secound = document.querySelector('.first-class');
+console.log(secound);
+
+const third = document.querySelectorAll('.third-class');
+console.log(third);
+
+const four = document.querySelectorAll('.byall');
+console.log(four);
