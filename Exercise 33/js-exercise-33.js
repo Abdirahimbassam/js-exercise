ee0cf9bb9812c69f51c37c1223ba86fbf474c6ce
abdirahimbassam <abdirahiimbassam@gmail.com>
2026-01-26 06:22:34 +0300

    Submitted Exercise 33 in javascript exercise

---
+++ Exercise 33/js-exercise-33.js
@@ -0,0 +1,16 @@
+
+const p1 = document.querySelector('#first');
+
+console.log(p1);
+
+const p2 = document.querySelector('.secound');
+
+console.log(p2);
+
+function ChangeName() {
+    p1.textContent = "DOM Exercise 33";
+}
+
+function ChangeCoding() {
+    p2.innerHTML = "Dugsiiye Mentorship";
+}
